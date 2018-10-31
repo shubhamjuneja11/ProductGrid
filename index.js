6862424708d8fef8716810b7a8daacef4732c5ed
Shubham Juneja <shubhamjuneja11@gmail.com>
2018-10-31 14:40:41 +0530

    Add Generic Filter

--- index.js
+++ index.js
@@ -1,86 +1,59 @@
 function ProductGrid(options) {
-  this.$brandsContainer = options.brandsContainer;
-  this.$colorsContainer = options.colorsContainer;
   this.$productsContainer = options.productsContainer;
-  this.stockAvailableCheckBox = options.stockAvailableCheckBox;
+  this.filtersContainer = options.filtersContainer;
   this.requestUrl = options.requestUrl;
-  this.brandClass = options.brandClass;
-  this.colorClass = options.colorClass;
-  this.productClass = options.productClass;
-  this.productImageClass = options.productImageClass;
-  this.productNameClass = options.productNameClass;
-  this.colorKey = options.colorKey;
-  this.brandKey = options.brandKey;
-  this.availableKey = options.availableKey;
-  this.availableTrueValue = options.availableTrueValue;
+  this.filterOptions = options.filterOptions;
+  this.filters = Object.keys(options.filterOptions);
+  this.product = options.product;
 }
 
 ProductGrid.prototype.init = function() {
-  this.fetchedBrands = [];
-  this.fetchedColors = [];
-  this.selectedBrands = [];
-  this.selectedColors = [];
   this.bindEventListeners();
+  this.filterData = {};
+  this.selectedFilters = {};
+  var _this = this;
+  $.each(this.filters, function(index, filter) {
+    _this.filterData[filter] = [];
+    _this.selectedFilters[filter] = [];
+  });
   this.fetchData();
 };
 
 /**************Event Binding***************************/
 ProductGrid.prototype.bindEventListeners = function() {
   var _this = this;
-  this.stockAvailableCheckBox.on('click', function() {
-    _this.filterStockAvailability(this);
-  });
-  this.$brandsContainer.on('click', 'input', function() {
-    _this.updateFilterArray(_this, this, _this.selectedBrands);
-  });
-  this.$colorsContainer.on('click', 'input', function() {
-    _this.updateFilterArray(_this, this, _this.selectedColors);
+  this.filtersContainer.on('click','input', function() {
+    var $this = $(this);
+      _this.updateSelectedFilters($this.attr('filter-name'),$this.attr('value'),$this.attr('filter-type'),$this[0].checked);
   });
 };
 
-ProductGrid.prototype.filterStockAvailability = function(checkBox) {
-  if (checkBox.checked) {
-    this.includeOnlyAvailableProducts = true;
+ProductGrid.prototype.updateSelectedFilters = function(filterName, filterValue, filterType, filterChecked) {
+ if(filterChecked) {
+    this.selectedFilters[filterName].push(filterValue);
   } else {
-    this.includeOnlyAvailableProducts = false;
+    this.selectedFilters[filterName].splice($.inArray(filterValue, this.selectedFilters[filterName]),1);
   }
   this.refreshProducts();
 };
 
-ProductGrid.prototype.updateFilterArray = function(gridContext, checkBox, filterArray) {
-  if (checkBox.checked) {
-    filterArray.push(checkBox.value);
-  } else {
-    filterArray.splice($.inArray(checkBox.value, filterArray), 1)
-  }
-  this.refreshProducts();
-};
-
-/*******************Filter data****************************/
-
-ProductGrid.prototype.filterData = function() {
-  var _this = this;
-  return this.productData.filter(function(product) {
-    return _this.shouldIncludeProduct(product);
-  });
-};
 
 ProductGrid.prototype.refreshProducts = function() {
-  this.setProductsData(this.filterData());
+  var _this = this;
+  this.filteredData = this.productData.filter(function(product) {
+    return _this.shouldIncludeProduct(product);
+  });
+  this.setProductsData();
 };
 
 ProductGrid.prototype.shouldIncludeProduct = function(product) {
   var includeProduct = true,
     _this = this;
-  if (includeProduct && this.includeOnlyAvailableProducts) {
-    includeProduct = (product[this.availableKey] === this.availableTrueValue);
-  }
-  if (includeProduct && this.selectedBrands.length > 0) {
-    includeProduct = $.inArray(product[this.brandKey], _this.selectedBrands) != -1;
-  }
-  if (includeProduct && this.selectedColors.length > 0) {
-    includeProduct = $.inArray(product[this.colorKey], _this.selectedColors) != -1
-  }
+  $.each(this.selectedFilters,function(filterName, filterValue) {
+    var productValue = product[filterName];
+    if(filterValue.length!=0)
+    includeProduct &= ($.inArray(productValue, filterValue)!=-1);
+  });
   return includeProduct;
 };
 
@@ -92,106 +65,125 @@ ProductGrid.prototype.fetchData = function() {
       dataType: "json"
     })
     .done(function(response) {
-      _this.productData = response;
+      _this.filteredData = _this.responseData = _this.productData = response;
       _this.setData();
     });
 };
 
 ProductGrid.prototype.setData = function() {
-  this.setFilterOptions();
-  this.setBrandsData();
-  this.setColorsData();
-  this.setProductsData(this.productData);
+  this.setProductsData();
+  this.setFiltersData();
+  this.createFilterViews();
 };
 
 /****************** Set Data*****************************/
-ProductGrid.prototype.setFilterOptions = function() {
+
+ProductGrid.prototype.setFiltersData = function() {
   var _this = this;
-  $.each(this.productData, function(index, value) {
-    var brand = value[_this.brandKey],
-      color = value[_this.colorKey];
-    if (_this.fetchedBrands.indexOf(brand) == -1) {
-      _this.fetchedBrands.push(brand);
-    }
-    if (_this.fetchedColors.indexOf(color) == -1) {
-      _this.fetchedColors.push(color);
-    }
+  $.each(this.productData, function(index, product) {
+    $.each(product, function(key, value) {
+      if($.inArray(key, _this.filters)!=-1 && ($.inArray(value, _this.filterData[key])==-1)) {
+        _this.filterData[key].push(value);
+      }
+    });
   });
 };
 
-ProductGrid.prototype.setBrandsData = function() {
-  var _this = this;
-  $.each(this.fetchedBrands, function(index, brandName) {
-    var newBrand = _this.createBrand(brandName);
-    _this.$brandsContainer.append(newBrand);
+ProductGrid.prototype.createFilterViews = function() {
+  var _this = this,
+    filterFragment = document.createDocumentFragment();
+  $.each(this.filterData, function(filterName, filterOptions) {
+    var filterType = _this.filterOptions[filterName]['filterType'];
+     if(filterType === 'boolean') {
+      var filterView = _this.createBooleanFilter(filterName);
+  } else {
+    var filterView = _this.createfilterView(filterName, filterOptions);
+  }
+    filterFragment.append(filterView);
   });
+  this.filtersContainer.append(filterFragment);
 };
 
-ProductGrid.prototype.setColorsData = function() {
-  var _this = this;
-  $.each(this.fetchedColors, function(index, colorName) {
-    var newColor = _this.createColor(colorName);
-    _this.$colorsContainer.append(newColor);
-  });
+ProductGrid.prototype.createfilterView = function(filterName, filterOptions) {
+    var filterViewTitle = this.createFilterTitle(filterName);
+    filterView = $('<div>'),
+    filterOptionsView = this.createFilterOptionsView(filterName, filterOptions)
+   return filterView.append(filterViewTitle).append(filterOptionsView).append('<br><br>').get(0);
 };
 
-ProductGrid.prototype.setProductsData = function(productsData) {
+ProductGrid.prototype.createBooleanFilter = function(filterName) {
+   var parentContainer = $('<div>'),
+     checkBoxView = $('<input>',{type:'checkbox',value:this.filterOptions[filterName]['trueValue'], id:filterName, 'filter-name': filterName, 'filter-type': 'boolean'}),
+     label = $('<label>', {for:filterName,text:this.filterOptions[filterName]['title']});
+    parentContainer.append(checkBoxView.get(0)).append(label).append('<br>');
+  return parentContainer.get(0);
+};
+
+ProductGrid.prototype.createFilterTitle = function(filterName) {
+  return $('<h2>').html(this.filterOptions[filterName]['viewTitle']).get(0);
+};
+
+ProductGrid.prototype.createFilterOptionsView = function(filterName, filterOptions) {
+  var parentContainer = $('<div>');
+  $.each(filterOptions, function(index, option) {
+    var checkBoxView = $('<input>',{type:'checkbox',value:option, id:option, 'filter-name': filterName, 'filter-type': 'multiple'}),
+     label = $('<label>', {for:option,text:option});
+    parentContainer.append(checkBoxView.get(0)).append(label).append('<br>');
+  });
+  return parentContainer.get(0);
+};
+
+ProductGrid.prototype.setProductsData = function() {
   var _this = this,
     fragment = document.createDocumentFragment();
-  $.each(productsData, function(index, value) {
-    fragment.append(_this.loadProductToView(index, value));
+  $.each(this.filteredData, function(index, value) {
+    fragment.append(_this.loadProductToView(value));
   });
   this.$productsContainer.html(fragment);
 };
 
 /*********Creating new views*****************/
 ProductGrid.prototype.createNewProduct = function() {
-  return $('<div>').addClass(this.productClass)
-    .append($('<div>')
-      .addClass(this.productImageClass)
-      .append('<img>'))
-    .append($('<div>')
-      .addClass(this.productNameClass));
+  return $('<div>')
+  .addClass(this.product['class'])
+    .append($('<div>').append('<img>'))
+    .append($('<div>',{'data-property': 'product-name'})) ;
 };
 
-ProductGrid.prototype.loadProductToView = function(index, product) {
+ProductGrid.prototype.loadProductToView = function(product) {
   return this.createNewProduct()
     .find('img')
     .attr('src', 'product_data/images/' + product['url'])
     .end()
-    .find('.product-name')
+    .find('[data-property="product-name"]')
     .text(product['name'])
     .end()
     .get(0);
 };
 
-ProductGrid.prototype.createBrand = function(brandName) {
-  return $('<div>').addClass(this.brandClass)
-    .html('<label for="' + brandName + '">' + brandName + '</label><input type="checkbox" value="' + brandName + '"">');
-};
-
-ProductGrid.prototype.createColor = function(colorName) {
-  return $('<div>').addClass(this.colorClass)
-    .html('<label for="' + colorName + '">' + colorName + '</label><input type="checkbox" value="' + colorName + '"">');
-};
 
 $(function() {
   var options = {
-    brandsContainer: $('[data-property=brands'),
-    colorsContainer: $('[data-property=colors]'),
-    productsContainer: $('[data-property=products'),
-    stockAvailableCheckBox: $('[data-property=available]'),
-    productClass: 'product-item',
-    productNameClass: 'product-name',
-    productImageClass: 'product-image',
     requestUrl: 'product.json',
-    brandClass: 'brand-item',
-    colorClass: 'color-item',
-    colorKey: 'color',
-    brandKey: 'brand',
-    availableKey: 'sold_out',
-    availableTrueValue: '0'
-  };
+    productsContainer: $('[data-property=products'),
+    filtersContainer: $('[data-property=filters]'),
+    filterOptions:  {'color': {
+      viewTitle: 'Colors'
+    },
+    'brand': {
+      viewTitle: 'Brands'
+    },
+    'sold_out': {
+      viewTitle: 'Available',
+      filterType: 'boolean',
+      trueValue: '0',
+      title: 'Exclude out of Stock'
+    }
+  },
+   product: {
+    class: 'product-item'
+   }
+  }
   var productGrid = new ProductGrid(options);
   productGrid.init();
 });
